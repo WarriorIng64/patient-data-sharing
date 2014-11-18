36f0fd743f2ba837d2253934cbaf16742d82d361
Christopher Kyle Horton <christhehorton@gmail.com>
2014-11-17 21:11:46 -0500

    Make v0.0.1 release (make_request completed, fixes #1 and #2).

--- suite.py
+++ suite.py
@@ -22,7 +22,7 @@
 import os
 
 SUITE_NAME = "patient-data-sharing"
-SUITE_VERSION = "v0.0.0"
+SUITE_VERSION = "v0.0.1"
 
 def check_file_exists(path):
     '''Checks whether a file exists, and quits the program if not.'''
